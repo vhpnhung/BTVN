20f9aa122c50683fc6af513d750f4873f1818a78
Nhung Vu <nhungvhp.03@gmail.com>
2022-08-20 22:21:48 +0700

    B3 L1

---
+++ weekday.py
@@ -0,0 +1,18 @@
+ngay = input("Nhap so tu 1-7: ")
+
+if ngay == "1":
+    print("Mon")
+elif ngay == "2":
+    print("Tue")
+elif ngay =="3":
+    print("Wed")
+elif ngay == "4":
+    print("Thu")
+elif ngay == "5":
+    print("Fri")
+elif ngay == "6":
+    print("Sat")
+elif ngay == "7":
+    print("Sun")
+else:
+    print("Try again?")
